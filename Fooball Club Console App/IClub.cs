22d1b51856811f876c3129ee148b398aedbd7326
Hussain Abbas <sayedhusainabbashaider78600@gmail.com>
2026-08-09 08:32:01 +0530

    Add project files and .gitignore for Football Club Console App

---
+++ Fooball Club Console App/IClub.cs
@@ -0,0 +1,10 @@
+﻿public interface IClub
+{
+    public IClub ViewSquad();
+    public IClub AddPlayer();
+    public IClub TrainPlayer();
+    public IClub CalculateWageBill();
+    public IClub AssignCaptain();
+    public IClub InitiateTransfer();
+    public IClub ViewPlayerProfile();
+}
